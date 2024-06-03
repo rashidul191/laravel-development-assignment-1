1d1d6b1b41db5639f87c6aa245c90273d53c9fa2
rashidul191 <rashidul.191cse.gub@gmail.com>
2024-06-03 22:52:54 +0600

    problem-2 done.

---
+++ problem2.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+Problem 2:
+Given a few paragraphs in a file, read the file and count how many words are there. 
+For example, if there is a file with the following contents:
+
+Nunc ex lorem, ullamcorper ut eleifend ac, pellentesque non dolor.  
+
+You need to output: 10
+
+Because there are 10 words. 
+Note: There can be multiple paragraphs. You need to count all of the words from all of the paragraphs. 
+You need to read the data from a file. 
+
+*/
+
+$otherFileTextData =  file_get_contents('simple.txt');
+
+
+function countWorlds($paragraphs)
+{
+    // return str_word_count($paragraphs);
+
+    $stringToArray = explode(' ', $paragraphs);
+    return count($stringToArray);
+}
+
+echo countWorlds($otherFileTextData);
