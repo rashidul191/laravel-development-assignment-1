9bc53b5c7016742968ef085e29800c40d77dd3a6
rashidul191 <rashidul.191cse.gub@gmail.com>
2024-06-04 23:57:22 +0600

    problem4 done.

--- problem4.php
+++ problem4.php
@@ -6,10 +6,27 @@ Problem 4:
 Print the following pattern based on the given number n (can be any number). 
 Sample input: 5 
 Sample output: 
-       *
-     ***
-   *****
+    *
+   ***
+  *****
  *******
 ********* 
 
 */
+
+
+function pattern($num)
+{
+
+    for ($i = 1; $i <= $num; $i++) {
+        for ($j = 1; $j <= $num - $i; $j++) {
+            echo " ";
+        }
+        for ($x = 1; $x <= 2*$i-1; $x++) {
+            echo "*";
+        }
+        echo "\n";
+    }
+}
+
+pattern(5);
